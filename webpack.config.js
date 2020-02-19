6d1e5a74020c17f24d3bc8d7a671cc64a6d5ee49
Danny Zamora <dannyzamora.js@gmail.com>
2020-02-18 21:21:22 -0800

    webpack fix database url

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,11 @@ const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-if(process.env.NODE_ENV === 'test'){
-    require('dotenv').config({path:'.env.test'})
-
-} else if (process.env.NODE_ENV === 'development'){
-    require('dotenv').config({path:'.env.development'})
-
-
-}
+if (process.env.NODE_ENV === 'test') {
+  require('dotenv').config({ path: '.env.test' });
+} else if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: '.env.development' });
+}  
 
 module.exports = (env) => {
     const isProduction = env === 'production';
@@ -64,10 +61,10 @@ module.exports = (env) => {
             new webpack.DefinePlugin({
                 'process.env.FIREBASE_API_KEY':JSON.stringify(process.env.FIREBASE_API_KEY),
                 'process.env.FIREBASE_AUTH_DOMAIN':JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-                'process.env.FIREBASE_DATABASE_URL':JSON.stringify(process.env.DATABASE_URL),
+                'process.env.FIREBASE_DATABASE_URL':JSON.stringify(process.env.FIREBASE_DATABASE_URL),
                 'process.env.FIREBASE_PROJECT_ID':JSON.stringify(process.env.FIREBASE_PROJECT_ID),
                 'process.env.FIREBASE_STORAGE_BUCKET':JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-                'process.env.FIREBASE_MESSAGING_SENDER_ID':JSON.stringify(process.env.MESSAGING_SENDER_ID),
+                'process.env.FIREBASE_MESSAGING_SENDER_ID':JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
                 'process.env.FIREBASE_APP_ID':JSON.stringify(process.env.FIREBASE_APP_ID),
                 'process.env.FIREBASE_MEASUREMENT_ID':JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID),
             })
